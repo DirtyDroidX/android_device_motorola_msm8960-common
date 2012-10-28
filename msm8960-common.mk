fff28377722778c431e4a77029ce026f4678993c
dhacker29 <davidhackerdvm@gmail.com>
2012-10-28 13:05:11 -0500

    move cm specific overlays to device
    Use prebuilt hwcomposer.msm8960

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -45,9 +45,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     copybit.msm8960 \
     gralloc.msm8960 \
-    hwcomposer.default \
     power.msm8960
 
+#    hwcomposer.msm8960
 # GPS
 #PRODUCT_PACKAGES += \
 #	gps.msm8960
