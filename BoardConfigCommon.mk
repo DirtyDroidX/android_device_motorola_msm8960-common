a491bf8385f015a654e3689bc593bc74cf3ae30a
dhacker29 <davidhackerdvm@gmail.com>
2012-11-14 16:14:47 -0600

    wifi module should just be wlan.ko

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,8 +36,8 @@ WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_WLAN_DEVICE                := qcwcn
-WIFI_DRIVER_MODULE_NAME          := prima_wlan
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/prima_wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := wlan
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
